2a4dd3a5457be9bb4d3e55aad485f6fa02806ab0
Remnants of Forgotten Disney <weber.austint@gmail.com>
2023-04-11 14:33:43 -0500

    adding test output

---
+++ include/debug.h
@@ -0,0 +1,7 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#define testVar 1000
+char intToHex(unsigned int);
+
+#endif
---
+++ source/debug.c
@@ -0,0 +1,7 @@
+char arr[16] = {
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
+};
+
+char intToHex(unsigned int num){
+    return arr[num];
+}
