92b92ae9aec14fed3897248a09518d2c409744ac
Starport75 <weber.austint@gmail.com>
2023-05-04 21:11:39 -0500

    Successfully tricked the game into starting trade

--- source/gb_link.cpp
+++ source/gb_link.cpp
@@ -21,12 +21,22 @@ void init() {
   interrupt_enable(INTR_SERIAL);
 }
 
+std::string convertToString(char* a, int size)
+{
+    int i;
+    std::string s = "";
+    for (i = 0; i < size; i++) {
+        s = s + a[i];
+    }
+    return s;
+}
+
 int start_link() {
   init();
 
+u32 SPIout = 0x0200FEFE;
+u32 SPIin = 0x00000000;
   bool firstTransfer = false;
-  bool async = false;
-  u32 counter = 0;
 
   while (true) {
     std::string output = "";
@@ -34,65 +44,38 @@ int start_link() {
 
     if (!linkSPI->isActive()) {
       firstTransfer = true;
-      output += "START: Set as Master\n";
-      output += "SELECT: Set as Slave\n";
-      output += "\n(stop: press L+R)\n";
-      output += "(hold A on start for async)\n";
-      output += "(hold B on start for waitMode)\n";
-      output +=
-          "\n\n\n\n\n\n\n\n\n\n\n\n[!] to test this demo...\n      "
-          "...use a GBC Link Cable!";
-
-      if ((keys & KEY_START) | (keys & KEY_SELECT)) {
-        // (3) Initialize the library
-        linkSPI->activate((keys & KEY_START) ? LinkSPI::Mode::MASTER_256KBPS
-                                             : LinkSPI::Mode::SLAVE);
-        linkSPI->setWaitModeActive(keys &
-                                   KEY_B);  // see `waitMode` in README.md
-        if (keys & KEY_A)
-          async = true;
-      }
+    linkSPI->activate(LinkSPI::Mode::SLAVE);
     } else {
       // Title
       auto modeName =
           linkSPI->getMode() == LinkSPI::Mode::SLAVE ? "[slave]" : "[master]";
       output += std::string(modeName) + "\n";
       if (firstTransfer) {
-        log(output + "Waiting...");
+        log(output + "Waiting for link cable...\nPress L and R to cancel");
         firstTransfer = false;
       }
 
-      if (!async) {
         // (4)/(5) Exchange 32-bit data with the other end
-        u32 remoteKeys = linkSPI->transfer(keys, []() {
+        SPIin = linkSPI->transfer(SPIout, []() {
+        bool exit = false;
           u16 keys = ~REG_KEYS & KEY_ANY;
-          return (keys & KEY_L) && (keys & KEY_R);
+          if ((keys & KEY_L) && (keys & KEY_R)){
+            exit = true;
+          }
+          return exit;
         });
-        output += "> " + std::to_string(keys) + "\n";
-        output += "< " + std::to_string(remoteKeys) + "\n";
-      } else {
-        // (6) Exchange data asynchronously
-        if (keys != 0 &&
-            linkSPI->getAsyncState() == LinkSPI::AsyncState::IDLE) {
-          counter++;
-          linkSPI->transferAsync(counter, []() {
-            u16 keys = ~REG_KEYS & KEY_ANY;
-            return (keys & KEY_L) && (keys & KEY_R);
-          });
-          log(output + ">> " + std::to_string(counter));
-          wait(228 * 60);
+        if (SPIin == 0x61616161){
+            SPIout = 0x61616161;
+        } else if ((SPIin & 0x0000FFFF) == 0x0000D1D1){
+            SPIout = 0xD1D1D1D1;
         }
-        if (linkSPI->getAsyncState() == LinkSPI::AsyncState::READY) {
-          log(output + "<< " + std::to_string(linkSPI->getAsyncData()));
-          wait(228 * 60);
-        }
-      }
+        output += "Send: " + std::to_string((SPIout)) + "\n";
+        output += "Recv: " + std::to_string((SPIin)) + "\n";
 
       // Cancel
       if ((keys & KEY_L) && (keys & KEY_R)) {
         linkSPI->deactivate();
-        async = false;
-        counter = 0;
+        return 0;
       }
     }
 
@@ -121,3 +104,5 @@ void wait(u32 verticalLines) {
     }
   };
 }
+
+
