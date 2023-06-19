fe528ed82f3e328cc7b1c8aab7ea56d01f7284d6
Starport75 <weber.austint@gmail.com>
2023-06-19 14:20:29 -0500

    Update pokemon.cpp

--- source/pokemon.cpp
+++ source/pokemon.cpp
@@ -1,5 +1,8 @@
 #include <tonc.h>
 
+#define pkmn_size(gen == 1 ? 44 : 48)
+#define name_size            11
+
 byte gen_one_pkmn_index[255];
 byte gen_two_pkmn_index[255];
 
@@ -7,13 +10,12 @@ byte gen_two_pkmn_index[255];
 class Pokemon{
 public:
 
-    void Pokemon(byte gen, byte party_data[441]){
+    void Pokemon(byte gen, int index, byte party_data[441]){
         num_of_pkmn = party_data[0x11];
 
-        for (int i = 0; i < num_of_pkmn; i++){
-        int party_offset = 20  + (i * 48);
-        int ot_offset    = 308 + (i * 11);
-        int name_offset  = 374 + (i * 11);
+        int party_offset = 20 + (index * 48);
+        int ot_offset    = 20 + (6 * pkmn_size) + (index * name_size);
+        int name_offset  = 20 + (6 * pkmn_size) + (6 * name_size) + (index * name_size);
             
         switch(gen){
             case 1:
@@ -22,24 +24,24 @@ public:
             break;
             case 2:
                 species_index = gen_one_pkmn_index[party_data[party_offset + 0x00];
-                moves[4];
-                trainer_id[2];
-                exp[3];
-                nickname[11];
-                trainer_name[11];
-                pokerus;
-                caught_data[2];
-                level;
+                std::copy(party_data[party_offset + 0x02], party_data[party_offset + 0x05], moves[0]);
+                std::copy(party_data[party_offset + 0x06], party_data[party_offset + 0x07], trainer_id[0];
+                exp[3]; //??
+                std::copy(party_data[name_offset + 0x00], party_data[name_offset + 0x0A], nickname[0]);
+                std::copy(party_data[ot_offset + 0x00], party_data[name_offset + 0x0A], trainer_name[11]);
+                pokerus = party_data[party_offset + 0x1C];
+                std::copy(party_data[party_offset + 0x1D], party_data[party_offset + 0x1C], caught_data[0]);
+                level; //??
             break;
-        }
+        
     }
 
     void convert_to_gen_three(){
-        byte gen_3_pkmn[80][6];
-    }
+        
+        {
+        
 private:
     byte gen;
-    byte num_of_pkmn;
     byte species_index;
     byte moves[4];
     byte trainer_id[2];
@@ -49,6 +51,7 @@ private:
     byte pokerus;
     byte caught_data[2];
     byte level;
+    byte gen_3_pkmn[80];
 
     }
 };
