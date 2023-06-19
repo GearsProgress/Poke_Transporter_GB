306ff6bb914cd525aa597d037547da2f47344346
Starport75 <weber.austint@gmail.com>
2023-06-19 12:31:49 -0500

    Update pokemon.cpp

--- source/pokemon.cpp
+++ source/pokemon.cpp
@@ -1,36 +1,54 @@
 #include <tonc.h>
 
-#define SPECIES_INDEX ((gen == 1) ? 0x01 : 0x02)
-#define MOVE_ONE
-#define MOVE_TWO
-#define MOVE_THREE
-#define MOVE_FOUR
-#define OT_TID
-#define EXP
-//#define IV_DATA
-#define LEVEL
-#define 
-
-
+byte gen_one_pkmn_index[255];
+byte gen_two_pkmn_index[255];
 
 
 class Pokemon{
 public:
-    byte pkmn_data[64];
-    int gen;
 
-    Pokemon(int generation){
-        gen = generation;
+    void Pokemon(byte gen, byte party_data[441]){
+        num_of_pkmn = party_data[0x11];
+
+        for (int i = 0; i < num_of_pkmn; i++){
+        int party_offset = 20  + (i * 48);
+        int ot_offset    = 308 + (i * 11);
+        int name_offset  = 374 + (i * 11);
+            
+        switch(gen){
+            case 1:
+          
+
+            break;
+            case 2:
+                species_index = gen_one_pkmn_index[party_data[party_offset + 0x00];
+                moves[4];
+                trainer_id[2];
+                exp[3];
+                nickname[11];
+                trainer_name[11];
+                pokerus;
+                caught_data[2];
+                level;
+            break;
+        }
     }
 
     void convert_to_gen_three(){
-        byte new_pkmn[100];
-
-
-
+        byte gen_3_pkmn[80][6];
     }
+private:
+    byte gen;
+    byte num_of_pkmn;
+    byte species_index;
+    byte moves[4];
+    byte trainer_id[2];
+    byte exp[3];
+    byte nickname[11];
+    byte trainer_name[11];
+    byte pokerus;
+    byte caught_data[2];
+    byte level;
 
-    byte get(){
-        return TEST;
     }
 };
