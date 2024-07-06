ce20ebfc3ac4c7312aad981d7c755b9137539b77
The Gears of Progress <weber.austint@gmail.com>
2024-07-05 20:53:59 -0400

    Prep for v0.2.4

--- include/debug_mode.h
+++ include/debug_mode.h
@@ -2,12 +2,12 @@
 #define DEBUG_MODE_H
 
 #define DEBUG_MODE false
-#define IGNORE_GAME_PAK true
-#define IGNORE_LINK_CABLE true
+#define IGNORE_GAME_PAK false
+#define IGNORE_LINK_CABLE false
 #define IGNORE_MG_E4_FLAGS false
-#define IGNORE_UNRECEIVED_PKMN true
+#define IGNORE_UNRECEIVED_PKMN false
 #define FORCE_TUTORIAL false
-#define DONT_REMOVE_PKMN true
+#define DONT_REMOVE_PKMN false
 #define SHOW_INVALID_PKMN false
 
 #define DEBUG_GAME SAPPHIRE_ID
